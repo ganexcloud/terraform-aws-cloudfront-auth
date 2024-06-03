2acd425fd99ad626667aa820d5eedaa1e5827a91
caiohasouza <caio@ganex.com.br>
2024-06-03 14:00:22 -0300

    fix: iam role assume role policy

--- lambda.tf
+++ lambda.tf
@@ -106,6 +106,18 @@ data "aws_iam_policy_document" "lambda_assume_role" {
       ]
     }
 
+    condition {
+      test     = "StringLike"
+      variable = "aws:SourceArn"
+      values   = ["arn:aws:lambda:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:function:${var.name}-auth"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "aws:SourceAccount"
+      values   = [data.aws_caller_identity.current.account_id]
+    }
+
     effect = "Allow"
   }
 }
